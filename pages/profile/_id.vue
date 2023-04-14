ecc19b08b9cd238c3c11bf4530b3959b15716cc8
Nate K <thekingfamilybiz@gmail.com>
2023-04-14 15:36:06 -0600

    got profile view set up and displaying members info

---
+++ pages/profile/_id.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>Profile</h1>
+    <div v-if="user">
+      <img :src="user.picture.large" alt="Profile picture" />
+      <h2>{{ user.name.first }} {{ user.name.last }}</h2>
+      <p>{{ user.location.street.number }} {{ user.location.street.name }}</p>
+      <p>{{ user.location.city }}, {{ user.location.state }} {{ user.location.postcode }}</p>
+      <p>Email: {{ user.email }}</p>
+      <p>Phone: {{ user.phone }}</p>
+      <p>Date of Birth: {{ formattedDateOfBirth }}</p>
+    </div>
+    <div v-else>
+      <p>Loading...</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import userService from '@/services/userService';
+
+export default {
+  data() {
+    return {
+      user: null,
+    };
+  },
+  computed: {
+    formattedDateOfBirth() {
+      if (this.user) {
+        const date = new Date(this.user.dob.date);
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        return date.toLocaleDateString('en-US', options);
+      }
+      return '';
+    },
+  },
+  async mounted() {
+    const userId = this.$route.params.id;
+    this.user = await userService.getUserById(userId);
+  },
+};
+</script>
