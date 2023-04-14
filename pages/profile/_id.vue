1121a25348d6e4d925e4803c4c4c933e88488833
Nate K <thekingfamilybiz@gmail.com>
2023-04-14 17:54:44 -0600

    fixed aformentioned bug where the view profile was generating a random user each time instead of the corresponding/correct user

--- pages/profile/_id.vue
+++ pages/profile/_id.vue
@@ -2,13 +2,10 @@
   <div>
     <h1>Profile</h1>
     <div v-if="user">
-      <img :src="user.picture.large" alt="Profile picture" />
+      <img :src="user. picture.large" alt="Profile picture" />
       <h2>{{ user.name.first }} {{ user.name.last }}</h2>
       <p>{{ user.location.street.number }} {{ user.location.street.name }}</p>
-      <p>
-        {{ user.location.city }}, {{ user.location.state }}
-        {{ user.location.postcode }}
-      </p>
+      <p>{{ user.location.city }}, {{ user.location.state }} {{ user.location.postcode }}</p>
       <p>Email: {{ user.email }}</p>
       <p>Phone: {{ user.phone }}</p>
       <p>Date of Birth: {{ formattedDateOfBirth }}</p>
@@ -20,7 +17,7 @@
 </template>
 
 <script>
-import userService from "@/services/userService";
+import userService from '@/services/userService';
 
 export default {
   data() {
@@ -32,17 +29,16 @@ export default {
     formattedDateOfBirth() {
       if (this.user) {
         const date = new Date(this.user.dob.date);
-        const options = { year: "numeric", month: "long", day: "numeric" };
-        return date.toLocaleDateString("en-US", options);
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        return date.toLocaleDateString('en-US', options);
       }
-      return "";
+      return '';
     },
   },
   async mounted() {
     const userId = this.$route.params.id;
-    console.log("User ID:", userId);
+    console.log(userId);
     this.user = await userService.getUserById(userId);
-    console.log("Fetched user:", this.user);
   },
 };
 </script>
--- services/userService.js
+++ services/userService.js
@@ -2,27 +2,34 @@ import axios from 'axios';
 
 const API_URL = 'https://randomuser.me/api/';
 
+let usersCache = null;
+
 export default {
   async getUsers() {
-    try {
-      const response = await axios.get(`${API_URL}?results=56`);
-      return response.data.results;
-    } catch (error) {
-      console.error('Error fetching users:', error.response || error);
-      throw new Error('Error fetching users from Random User API');
+    if (!usersCache) {
+      try {
+        const response = await axios.get(`${API_URL}?results=56`);
+        usersCache = response.data.results;
+      } catch (error) {
+        console.error('Error fetching users:', error.response || error);
+        throw new Error('Error fetching users from Random User API');
+      }
     }
+    return usersCache;
   },
 
   async getUserById(id) {
-    try {
-      const response = await axios.get(`${API_URL}?results=56`);
-      const users = response.data.results;
-      const user = users.find((user) => user.login.uuid === id);
+    if (!usersCache) {
+      await this.getUsers();
+    }
 
-      return user;
-    } catch (error) {
-      console.error('Error fetching user by ID:', error);
+    const user = usersCache.find((user) => user.login.uuid === id);
+
+    if (!user) {
+      console.error('User not found by ID:', id);
       return null;
     }
+
+    return user;
   },
 };
